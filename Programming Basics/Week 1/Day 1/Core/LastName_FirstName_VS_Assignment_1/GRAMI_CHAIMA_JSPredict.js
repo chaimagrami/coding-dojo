1db3de1c9c06f52a56e509f1f6466f7242590edc
chaimagrami <chaimagrami53@gmail.com>
2023-10-17 11:16:25 +0100

    commit

---
+++ Programming Basics/Week 1/Day 1/Core/LastName_FirstName_VS_Assignment_1/GRAMI_CHAIMA_JSPredict.js
@@ -0,0 +1,22 @@
+function myBirthYearFunc(){
+        console.log("I was born in " + 1980);
+    }
+// when the myBirthYearFunc function is called, it will log: "I was born in 1980" .
+function myBirthYearFunc(birthYearInput){
+        console.log("I was born in " + birthYearInput);
+    }
+// Also when the myBirthYearFunc function is called, it will log: "I was born in 1980" .    
+function add(num1, num2){
+        console.log("Summing Numbers!");
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+//when the add function is called, it will log:
+//Summing Numbers!
+//num1 is: 10
+//num2 is: 20
+//30
+    
+myBirthYearFunc()
