01900096ac2649e78a9e228672ec21fc266a20fb
chaimagrami <chaimagrami53@gmail.com>
2023-11-28 15:30:13 +0100

    dojo survey

--- Python/Week 1/Day 5/Core/dojo survey/server.py
+++ Python/Week 1/Day 5/Core/dojo survey/server.py
@@ -1,19 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,redirect, session
 
 app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         # Get form data
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
+        session['name'] = request.form['name']
+        session['DojoLocation'] = request.form['DojoLocation']
+        session['FavoriteLanguage'] = request.form['FavoriteLanguage']
 
         # Display form data on the result page
-        return render_template('result.html', name=name, email=email, message=message)
+        return redirect('/result')
 
     return render_template('index.html')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
